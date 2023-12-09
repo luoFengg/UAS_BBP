5d3214da0353ac5462b5a1c6b89610c3db0332ed
Luo Feng <151534931+luoFengg@users.noreply.github.com>
2023-12-09 14:37:02 +0700

    utility.js

--- js/utility.js
+++ js/utility.js
@@ -1,48 +1,27 @@
 function attackBox({ kotak1, kotak2 }) {
   return (
-    kotak1.getAttackBoxX() + kotak1.getAttackBoxWidth() >=
-      kotak2.getAttackBoxX() &&
-    kotak1.getAttackBoxX() <=
-      kotak2.getAttackBoxX() + kotak2.getAttackBoxWidth() &&
-    kotak1.getAttackBoxY() + kotak1.getAttackBoxHeight() >= kotak2.posisi.y &&
-    kotak1.getAttackBoxY() <= kotak2.posisi.y + kotak2.height
+    kotak1.attackBoxX + kotak1.attackBoxWidth >= kotak2.attackBoxX &&
+    kotak1.attackBoxX <= kotak2.attackBoxX + kotak2.attackBoxWidth &&
+    kotak1.attackBoxY + kotak1.attackBoxHeight >= kotak2.posisiY &&
+    kotak1.attackBoxY <= kotak2.posisiY + kotak2.height
   );
 }
 
-function menentukanYangMenang({ player, enemy, timerId }) {
+let timerId;
+function menentukanYangMenang() {
   clearTimeout(timerId);
   document.getElementById("displayText").style.display = "flex";
 
-  if (player.getHealth() === enemy.getHealth()) {
+  if (player.health === enemy.health) {
     document.getElementById("displayText").innerHTML = "Seri";
-  } else if (player.getHealth() > enemy.getHealth()) {
+  } else if (player.health > enemy.health) {
     document.getElementById("displayText").innerHTML = "Player 1 Menang!";
   } else {
     document.getElementById("displayText").innerHTML = "Player 2 Menang!";
   }
 }
 
-function wallDetection() {
-  if (player.posisi.x < 0) {
-    player.posisi.x = 0;
-  } else if (player.posisi.x + player.width > canvas.width) {
-    player.posisi.x = canvas.width - player.width;
-  } else if (player.posisi.y < 0) {
-    player.posisi.y = 0;
-    player.setVelocityY(0);
-  }
-
-  if (enemy.posisi.x < 0) {
-    enemy.posisi.x = 0;
-  } else if (enemy.posisi.x + enemy.width > canvas.width) {
-    enemy.posisi.x = canvas.width - enemy.width;
-  } else if (enemy.posisi.y < 0) {
-    enemy.posisi.y = 0;
-    enemy.setVelocityY(0);
-  }
-}
 let timer = 60;
-let timerId;
 function decreaseTimer() {
   timerId = setTimeout(decreaseTimer, 1000);
   if (timer > 0) {
@@ -51,6 +30,27 @@ function decreaseTimer() {
   }
 
   if (timer === 0) {
-    menentukanYangMenang({ player, enemy, timerId });
+    menentukanYangMenang();
   }
 }
+
+function wallDetection() {
+  if (player.posisiX < 0) {
+    player.posisiX = 0;
+  } else if (player.posisiX + player.width > canvas.width) {
+    player.posisiX = canvas.width - player.width;
+  } else if (player.posisiY < 0) {
+    player.posisiY = 0;
+    player.velocityY = 0;
+  }
+
+  if (enemy.posisiX < 0) {
+    enemy.posisiX = 0;
+  } else if (enemy.posisiX + enemy.width > canvas.width) {
+    enemy.posisiX = canvas.width - enemy.width;
+  } else if (enemy.posisiY < 0) {
+    enemy.posisiY = 0;
+    enemy.velocityY = 0;
+  }
+}
+
